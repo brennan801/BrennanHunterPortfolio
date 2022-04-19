422fc292615204c1a947bdf4dd361261ad201e42
brennan801 <brennan.hunter@gmail.com>
2022-04-18 23:39:49 -0600

    Swag story

---
+++ BrennanHunterPortfolio/Pages/Blogs/Swag.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BrennanHunterPortfolio.Pages.Blogs
+{
+    public class SwagModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
