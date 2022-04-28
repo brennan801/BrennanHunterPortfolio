ddb8dc84d3fcea33fdc81fadd9067e22347c06d4
brennan801 <brennan.hunter@gmail.com>
2022-04-27 21:02:36 -0600

    CICD blog

---
+++ BrennanHunterPortfolio/Pages/Blogs/CICD.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BrennanHunterPortfolio.Pages.Blogs
+{
+    public class CICDModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
