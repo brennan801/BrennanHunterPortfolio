762527fa4230d4c30c3498fd6d8cb7593d935f56
brennan801 <brennan.hunter@gmail.com>
2022-04-30 19:32:48 -0600

    CapstoneBlog

---
+++ BrennanHunterPortfolio/Pages/ProjectStories/CapStone.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BrennanHunterPortfolio.Pages.ProjectStories
+{
+    public class CapStoneModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
