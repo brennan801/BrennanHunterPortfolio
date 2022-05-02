46eaf1450011a4b335eace0acddfacdc9c0cacf9
brennan801 <brennan.hunter@gmail.com>
2022-05-02 17:19:48 -0600

    github secrets blog

---
+++ BrennanHunterPortfolio/Pages/Blogs/GithubSecrets.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BrennanHunterPortfolio.Pages.Blogs
+{
+    public class GithubSecretsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
